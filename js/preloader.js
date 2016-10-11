7bd27fe4309d3e26e5ee57f18815af84a00caba7
FamSlayer <fuller.rules@gmail.com>
2016-10-11 15:26:09 -0400

    Continuing to separate level generation

--- js/preloader.js
+++ js/preloader.js
@@ -5,18 +5,43 @@ function preload()
     game.load.audio('frankenstein', 'assets/music/frankenstein.mp3');
 
     game.load.spritesheet("player_sprite", "assets/run.png", 96, 144);
-    game.load.image("test_ground_short", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat1.png");
-    game.load.image("test_ground_med", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat3.png");
-    game.load.image("test_ground_med2", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat4.png");
-    game.load.image("test_ground_long", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat2.png");
-    game.load.image("fire1", "assets/placeholder/fire1.png");
-    game.load.image("fire2", "assets/placeholder/fire2.png");
-    game.load.image("fire3", "assets/placeholder/fire3.png");
     
-    // these are the quick background layers I had to make
-    game.load.image("layer0", "assets/Background Layers/Layer 0/Lvl_1.png");
-    game.load.image("layer1", "assets/Background Layers/Layer 1/Lvl_1.png");
-    game.load.image("layer2", "assets/Background Layers/Layer 2/Lvl_1.png");
+    
+    // these are the platforms for lv 1
+    game.load.image("lv1_ground_short", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat1.png");
+    game.load.image("lv1_ground_med1", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat3.png");
+    game.load.image("lv1_ground_med2", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat4.png");
+    game.load.image("lv1_ground_long", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat2.png");
+
+    // these are the platforms for lv 2
+    game.load.image("lv2_ground_short", "assets/All_Platforms/Resized_WholeGreen/Green_Plat2.png");
+    game.load.image("lv2_ground_med1", "assets/All_Platforms/Resized_WholeGreen/Green_Plat1.png");
+    game.load.image("lv2_ground_med2", "assets/All_Platforms/Resized_WholeGreen/Green_Plat4.png");
+    game.load.image("lv2_ground_long", "assets/All_Platforms/Resized_WholeGreen/Green_Plat3.png");
+
+    // these are the platforms for lv 2
+    game.load.image("lv3_ground_short", "assets/All_Platforms/Resized_WholeRed/Red_Plat3.png");
+    game.load.image("lv3_ground_med1", "assets/All_Platforms/Resized_WholeRed/Red_Plat1.png");
+    game.load.image("lv3_ground_med2", "assets/All_Platforms/Resized_WholeRed/Red_Plat2.png");
+    game.load.image("lv3_ground_long", "assets/All_Platforms/Resized_WholeRed/Red_Plat4.png");
+
+
+    // these are the background layers for lv 1
+    game.load.image("lv1_layer0", "assets/Background Layers/Layer 0/Lvl_1.png");
+    game.load.image("lv1_layer1", "assets/Background Layers/Layer 1/Lvl_1.png");
+    game.load.image("lv1_layer2", "assets/Background Layers/Layer 2/Lvl_1.png");
+
+    // these are the background layers for lv 2
+    game.load.image("lv2_layer0", "assets/Background Layers/Layer 0/Lvl_2.png");
+    //game.load.image("lv2_layer1", "assets/Background Layers/Layer 1/Lvl_2.png");
+    game.load.image("lv2_layer2", "assets/Background Layers/Layer 2/Lvl_2.png");
+
+    // these are the background layers for lv 3
+    game.load.image("lv3_layer0", "assets/Background Layers/Layer 0/Lvl_3.png");
+    //game.load.image("lv3_layer1", "assets/Background Layers/Layer 1/Lvl_3.png");
+    game.load.image("lv3_layer2", "assets/Background Layers/Layer 2/Lvl_3.png");
+
+
 
 	
 	game.load.image("textBox", "assets/placeholder/dialogue.png");
