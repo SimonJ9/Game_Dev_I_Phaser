b9f409d2f1afd6b2e24439641bf3e209350514ed
FamSlayer <fuller.rules@gmail.com>
2016-10-11 00:45:51 -0400

    Increased gravity to 700 from 600

--- js/preloader.js
+++ js/preloader.js
@@ -3,18 +3,19 @@
 function preload()
 {
     game.load.spritesheet("test_dude", "assets/placeholder/dude.png", 32, 48);
-    game.load.image("test_ground_short", "assets/testing platforms_small.png");
-    game.load.image("test_ground_med", "assets/testing platforms.png");
-    game.load.image("test_ground_long", "assets/testing platforms_big.png");
+    game.load.image("test_ground_short", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat1.png");
+    game.load.image("test_ground_med", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat3.png");
+    game.load.image("test_ground_med2", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat4.png");
+    game.load.image("test_ground_long", "assets/All_Platforms/Resized_WholeBlue/Blue_Plat2.png");
     game.load.image("fire1", "assets/placeholder/fire1.png");
     game.load.image("fire2", "assets/placeholder/fire2.png");
     game.load.image("fire3", "assets/placeholder/fire3.png");
     
     // these are the quick background layers I had to make
-    game.load.image("layer0", "assets/Sample Backgrounds/layer0.png");
+    game.load.image("layer0", "assets/Background Layers/Layer 0/Lvl_3.png");
     game.load.image("layer1", "assets/Sample Backgrounds/layer1.png");
-    game.load.image("layer2", "assets/Sample Backgrounds/layer2.png");
-	
+    game.load.image("layer2", "assets/Background Layers/Layer 2/Lvl_3.png");
+
 	
 	game.load.image("textBox", "assets/placeholder/dialogue.png");
 	//game.load.bitmapFont("8bit_wonder", "assets/8-BIT WONDER.TTF");
