095782b17f286a494eba2afd5f380714af380f38
FamSlayer <fuller.rules@gmail.com>
2016-10-12 12:05:32 -0400

    Oh shit

---
+++ js/Levels/Game Over.js
@@ -0,0 +1,50 @@
+// JavaScript source code
+// JavaScript source code
+var GameOver = function (game) {
+
+}
+GameOver.prototype = {
+
+    /* preload function */
+    preload: function () {
+        this.load.audio('song', 'assets/music/other_song.mp3');
+        this.load.spritesheet("screen", "assets/Game Over/GameOver_SpriteSheet.png", 1334, 750);
+
+        this.input.mouse.capture = true;    // track the mouse
+        //Text
+        box = this.load.image("textBox", "assets/placeholder/dialogue.png");
+        show = false;
+        //this.load.bitmapFont("8bit_wonder", "assets/8-BIT WONDER.TTF");
+
+    },
+
+    /* initialization function */
+    create: function () {
+        this.screen = this.add.sprite(0, 0, "screen");
+        this.screen.animations.add("blink", [1, 1, 1, 1, 0, 0], 5, true);
+        this.screen.animations.play("blink");
+
+        main_menu_music = this.add.audio('song');
+        main_menu_music.play();
+    },
+
+    /* update loop */
+    update: function () {
+        if (this.input.activePointer.leftButton.isDown) {
+            if(global_current_level === 1)
+            {
+                this.state.start("Level_1");
+            }
+            else if(global_current_level === 2)
+            {
+                this.state.start("Level_2");
+            }
+            else if(global_current_level === 3)
+            {
+                this.state.start("Level_3");
+            }
+        }
+
+    }
+};
+
